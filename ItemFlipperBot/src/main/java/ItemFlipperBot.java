189dcb367556106c63177b65210b1b6a669a577a
Adam <bytelorde@gmail.com>
2021-05-06 19:27:11 -0700

    get nova set up as ad ependency

--- ItemFlipperBot/src/main/java/ItemFlipperBot.java
+++ ItemFlipperBot/src/main/java/ItemFlipperBot.java
@@ -1,9 +1,10 @@
+
 import nova.SingleStateNovaBot;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 
 
-@ScriptManifest(name = "Grand Exchange Bot", description = "", author = "Syndicate", version = 1.01, category = Category.MISC, image = "")
+@ScriptManifest(name = "Pure essence Flipper", description = "", author = "Syndicate", version = 1.01, category = Category.MISC, image = "")
 public class ItemFlipperBot extends SingleStateNovaBot {
 
     public ItemFlipperBot() {
