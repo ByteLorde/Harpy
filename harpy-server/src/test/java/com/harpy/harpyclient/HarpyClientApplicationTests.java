a4f2f4cc3980e343ab60aec845c0ee19601ed241
Adam <bytelorde@gmail.com>
2021-05-09 02:22:09 -0700

    create harpy client and servers

---
+++ harpy-server/src/test/java/com/harpy/harpyclient/HarpyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harpy.harpyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarpyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
