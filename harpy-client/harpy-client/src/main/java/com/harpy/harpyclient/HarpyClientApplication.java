bfed0afeae07e87cd3a35c4ffa71b756c326b3bb
Adam <bytelorde@gmail.com>
2021-05-09 02:04:44 -0700

    initial create harpy client

---
+++ harpy-client/harpy-client/src/main/java/com/harpy/harpyclient/HarpyClientApplication.java
@@ -0,0 +1,13 @@
+package com.harpy.harpyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarpyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarpyClientApplication.class, args);
+	}
+
+}
---
+++ harpy-client/harpy-client/src/test/java/com/harpy/harpyclient/HarpyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harpy.harpyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarpyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
